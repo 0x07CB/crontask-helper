c3e92b08ca0ce9ee5beaeefd51a5e78f42efdafe
0x07CB <83157348+0x07CB@users.noreply.github.com>
2025-03-02 18:37:32 +0100

    Amélioration de la gestion des paramètres None dans write_formatted_crontask
    - Modification de la logique de vérification des paramètres None et vides
    - Ajout de l'importation de Union depuis typing
    - Ajustement des conditions de validation pour les paramètres d'entrée
    - Simplification de la conversion des paramètres avec des vérifications de type et de longueur

--- crontask-helper/functions.py
+++ crontask-helper/functions.py
@@ -4,7 +4,7 @@
 
 #from typing import List
 #from typing import Dict
-#from typing import Union
+from typing import Union
 #from typing import Tuple
 from typing import Optional
 #from typing import Any
@@ -18,11 +18,11 @@ class CrontaskFormats:
   SpecialStrings = ["@reboot", "@hourly", "@daily", "@midnight", "@weekly", "@monthly", "@yearly"]
 
 def write_formatted_crontask(
-    Minute: Optional[str] = "*",
-    Hour: Optional[str] = "*",
-    Day: Optional[str] = "*",
-    Month: Optional[str] = "*",
-    Weekday: Optional[str] = "*",
+    Minute: Optional[str] = None,
+    Hour: Optional[str] = None,
+    Day: Optional[str] = None,
+    Month: Optional[str] = None,
+    Weekday: Optional[str] = None,
     command_to_be_executed: Optional[str] = "{{command}}"
 ) -> str:
   """
@@ -41,11 +41,11 @@ def write_formatted_crontask(
   """
   # Convertir tous les arguments en chaînes de caractères et remplacer les valeurs vides ou None par '*'
   params = {
-    'Minute': '*' if isinstance(Minute, None) else Minute,
-    'Hour': '*' if isinstance(Hour, None) else Hour,
-    'Day': '*' if isinstance(Day, None) else Day,
-    'Month': '*' if isinstance(Month, None) else Month,
-    'Weekday': '*' if isinstance(Weekday, None) else Weekday,
+    'Minute': '*' if not isinstance(Minute, str) or len(Minute) < 1 else Minute,
+    'Hour': '*' if not isinstance(Hour, str) or len(Hour) < 1 else Hour,
+    'Day': '*' if not isinstance(Day, str) or len(Day) < 1 else Day,
+    'Month': '*' if not isinstance(Month, str) or len(Month) < 1 else Month,
+    'Weekday': '*' if not isinstance(Weekday, str) or len(Weekday) < 1 else Weekday,
     'command_to_be_executed': command_to_be_executed
   }
   
